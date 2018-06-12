b1ca0de125fdc0897837d80654c71e9700dc58f5
te25son <te25son@gmail.com>
2018-06-12 22:10:52 +0200

    got rid of the redundant i

---
+++ more_testing/More_randoms.py
@@ -0,0 +1,23 @@
+"""
+More random stuff.
+I'm really just playing around now trying to get a hang of what all this is about.
+"""
+
+print('Good evening young sirs.')
+
+def space():
+    """
+    When called, prints a new line
+    """
+    print('\n')
+
+space()
+
+for x in range(10):
+    print('Python')
+
+space()
+
+my_list = ['I', 'wish', 'I', 'could', 'practice', 'this', 'in', 'a', 'real', 'setting', '.']
+for x in range(len(my_list)):
+    print(my_list[x])
