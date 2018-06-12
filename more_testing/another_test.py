d4b3ac085f6220a700308a4950c333b74ab2ca1f
te25son <te25son@gmail.com>
2018-06-12 21:01:27 +0200

    this is a test to push the file to github...again

---
+++ more_testing/another_test.py
@@ -0,0 +1,13 @@
+"""
+This is a test to see how things work, when I create a new directory in Pycharm then push it to Github
+"""
+
+print('Did it work?')
+
+color_list = ['red', 'green', 'blue', 'purple', 'yellow']
+
+print('')
+
+for i in range(len(color_list)):
+    print(color_list[i])
+    i + 1
