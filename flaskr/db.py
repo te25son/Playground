8ffe22b25ec7b37b11324936afdae22b83f64d58
te25son <te25son@gmail.com>
2018-06-24 16:58:16 +0200

    oops forgot some stuff...

--- flaskr/db.py
+++ flaskr/db.py
@@ -28,3 +28,11 @@ def init_db():
 
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf8'))
+
+
+@click.command('init-db')
+@with_appcontext
+def init_db_command():
+    """Clear the existing data and create new tables."""
+    init_db()
+    click.echo('Initialized the database.')
